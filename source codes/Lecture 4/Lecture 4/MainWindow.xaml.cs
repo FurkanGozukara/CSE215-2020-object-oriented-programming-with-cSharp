a9f7ff8eb86f4bc0337a51e8ae10f7d67bd79b6c
Furkan Gözükara <furkangozukara@gmail.com>
2020-10-28 00:43:47 +0300

    lecture 4 code upload

---
+++ source codes/Lecture 4/Lecture 4/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lecture_4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        //very good article about reference types value types
+        //heap stack garbage collector
+        //https://www.c-sharpcorner.com/article/C-Sharp-heaping-vs-stacking-in-net-part-i/
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //random is class therefore it is a reference type
+        static Random myRand = new Random();
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (GC.TryStartNoGCRegion(199999999, true))
+            {
+                try
+                {
+                    for (int i = 0; i < 100000000; i++)
+                    {
+                        //double is a struct therefore it is a value type
+                        double dblVal = myRand.NextDouble();
+                    }
+                }
+                finally
+                {
+
+                }
+            }
+
+           
+        }
+
+        public class myDoubleClas
+        {
+            public double dblVal;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (GC.TryStartNoGCRegion(199999999, true))
+            {
+                try
+                {
+                    for (int i = 0; i < 100000000; i++)
+                    {
+                        testReferenceTypeCall();
+                    }
+                }
+                finally
+                {
+                   
+                }
+            }
+
+          
+        }
+
+        private void testReferenceTypeCall()
+        {
+            myDoubleClas dblVal = new myDoubleClas { dblVal = myRand.NextDouble() };
+        }
+    }
+}
---
+++ source codes/Lecture 4/Lecture 4/value types.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lecture_4
+{
+    class value_types
+    {
+        bool blValueType;
+        byte byteType;// default = 0
+        char crType;//'/0' null as character
+        decimal dcType;// default = 0
+        double dblType;// default = 0
+        enum enType // default = empty
+        {
+
+        };
+        float flType; // default = 0
+int irType; // default = 0 default = 0
+        long lnType; // default = 0
+        sbyte sbyteType; // default = 0
+        short srType; // default = 0
+        struct strucrtpe // default = empty
+        {
+
+        };
+        uint untype; // default = 0
+        ulong utype;// default = 0
+        ushort ushortype;// default = 0
+    }
+
+    class referenceTypes
+    {
+        // by default null without initialization
+        class csType 
+        {
+
+        };
+
+        // by default null without initialization
+        interface Animal
+        {
+            void animalSound(); // interface method (does not have a body)
+            void run(); // interface method (does not have a body)
+        }
+
+        // by default null without initialization
+        public delegate void MyDelegate(string msg); // declare a delegate
+
+        // by default null without initialization
+        object obj;
+
+        // by default null without initialization
+        string sr;
+    }
+}
