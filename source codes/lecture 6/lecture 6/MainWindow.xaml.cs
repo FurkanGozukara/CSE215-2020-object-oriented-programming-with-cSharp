cd59647f63164e552a2df827b4b5af7080b464a3
Furkan Gözükara <furkangozukara@gmail.com>
2020-11-10 20:54:31 +0300

    lecture 6 source code

---
+++ source codes/lecture 6/lecture 6/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static lecture_6.staticMethods;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace lecture_6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //field versus properties : https://stackoverflow.com/questions/295104/what-is-the-difference-between-a-field-and-a-property
+
+        private void btnComposeCarObjects_Click(object sender, RoutedEventArgs e)
+        {
+            cars car_1 = new cars();
+
+            cars car_2 = new cars { irCarPrice = 250, irCarWeight = 250, srCarBrand = "BMW", srCarColor = "Red", srCarModel = "BMW 228", irProductionYear = 1825, lstRepairYears = new List<int> { 1775, 1900, 1645, 1925, 1999 } };
+
+            car_2.saveToAFile("car2.txt");
+
+            //MessageBox.Show("production year " + car_2.irProductionYear);
+
+            cars car_3 = new cars();
+
+            car_3.irCarPrice = 5000;
+            car_3.irCarWeight = 2500;
+            car_3.irProductionYear = 2000;
+            car_3.srCarBrand = "Ford";
+            car_3.srCarColor = "White";
+            car_3.srCarModel = "Ford Escape Hybrid";
+            car_3.lstRepairYears = new List<int>();
+            car_3.lstRepairYears.Add(1950);
+            car_3.lstRepairYears.Add(2005);
+
+
+            List<cars> lstMyCars = new List<cars> { car_1, car_2, car_3 };
+
+            lstMyCars.saveToFileCarsList("cars_list.txt");
+
+            File.WriteAllText(@"cars_list_json.txt", JsonConvert.SerializeObject(lstMyCars));
+        }
+
+        private void btnTestExceptions_Click(object sender, RoutedEventArgs e)
+        {
+            int ir1 = "sadasd1".toInt();
+            int ir2 = "2".toInt();
+            int ir3 = "2453465467546356563".toInt();
+        }
+
+        private void btnLoadCarFromFile_Click(object sender, RoutedEventArgs e)
+        {
+            cars myCar2 = new cars();
+            myCar2.loadFromFile("car2.txt");
+
+            List<cars> lstMyCars = new List<cars>();
+
+            lstMyCars.loadCarsFromFile("cars_list.txt");
+
+            List<cars> lstMyCars_json = new List<cars>();
+
+            lstMyCars_json = JsonConvert.DeserializeObject<List<cars>>(File.ReadAllText("cars_list_json.txt"));
+        }
+
+        private void btnTestReadOnlyandConstant_Click(object sender, RoutedEventArgs e)
+        {
+            //this below would throw error because you cant change value of readonly object after intilization or constructor of static class
+            //srSplitValueSeperator = "custom stuff";
+
+            //this below would throw error because you cant change value of constant object after first definition
+            //irConstNum = 200;
+
+            //cars.srCarManufacturer this would throw error
+            cars myTempCar = new cars();
+            var carm = myTempCar.srCarManufacturer;
+
+            var carm2 = cars.srCarManufacturer_v2;
+
+            var carm3 = cars.srCarManufacturer_v3;
+
+            cars.srCarManufacturer_v3 = "Global v2";
+
+            //constant vs readonly https://www.pluralsight.com/guides/const-vs-readonly-vs-static-csharp
+        }
+    }
+}
---
+++ source codes/lecture 6/lecture 6/staticMethods.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace lecture_6
+{
+    public static class staticMethods
+    {
+        public static readonly string srSplitValueSeperator = "split".ComputeSha256Hash();
+
+        public static readonly string srSplitListSeperator = "splitList".ComputeSha256Hash();
+
+        public static readonly string srCarObjectSeperator = "srCarObjectSeperator".ComputeSha256Hash();
+
+        //this below would throw error because const is required to be initialized at the begining different than static
+        //public const string srRandomSplitKey;
+
+        //this below one works unlike above, because it doesnt have to be assigned to a value at the first definition
+        //public static string srRandomSplitKey;
+
+        public const int irConstNum = 100;
+
+        //this is the only way to change static readonly
+        //static staticMethods ()
+        //{
+        //    srSplitValueSeperator = "";
+        //}
+
+        static string ComputeSha256Hash(this string rawData)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        //when you put this keyword in front of the parameter class type, that method becomes an extension
+        //extensions can be defined only as static
+        public static int toInt(this string srValue)
+        {
+            try
+            {
+                return Convert.ToInt32(srValue);
+            }
+            catch (FormatException E)
+            {
+                return -1;
+            }
+            catch (OverflowException E)
+            {
+                return Int32.MaxValue;
+            }
+            catch
+            {
+                //catch all other exceptions
+                return 0;
+            }
+        }
+
+
+        public static List<int> toIntList(this string srValue, string srSplitKey = null)
+        {
+            if (string.IsNullOrEmpty(srSplitKey))
+                srSplitKey = srSplitListSeperator;
+
+            List<int> lstReturnList = new List<int>();
+
+            foreach (var vrPerSplit in srValue.Split(srSplitKey))
+            {
+                lstReturnList.Add(vrPerSplit.toInt());
+            }
+
+            return lstReturnList;
+        }
+
+        public class cars
+        {
+            public static string srCarManufacturer_v3 = "Global";
+
+            public const string srCarManufacturer_v2 = "Global";
+
+            public readonly string srCarManufacturer = "Global";
+
+            //field
+            public string srCarBrand = "NA";
+
+            public int irCarPrice { get; set; }
+
+            public string srCarColor = "NA";
+
+            private int _irCarWeight;
+
+            //property
+            public int irCarWeight
+            {
+                get { return _irCarWeight; }   // get method
+
+                set
+                {
+                    _irCarWeight = value;
+
+                    if (_irCarWeight < 500)
+                        _irCarWeight = 500;
+
+                }  // set method
+            }
+
+            public string srCarModel { get; set; }
+
+            private int _irProductionYear;
+
+            public int irProductionYear
+            {
+                get
+                {
+                    if (_irProductionYear < 1850)
+                        return 1850;
+
+                    return _irProductionYear;
+                }
+
+                set { _irProductionYear = value; }
+            }
+
+            List<int> _lstRepairYears = new List<int>();
+
+            public List<int> lstRepairYears
+            {
+                get
+                {
+                    for (int i = 0; i < _lstRepairYears.Count; i++)
+                    {
+                        if (_lstRepairYears[i] < this.irProductionYear)
+                            _lstRepairYears[i] = this.irProductionYear;
+                    }
+
+                    return _lstRepairYears;
+                }
+
+                set
+                {
+                    _lstRepairYears = value;
+                }
+            }
+
+            public void saveToAFile(string srFileName)
+            {
+                File.WriteAllText(srFileName, returnTextFormatOfObject());
+            }
+
+            public string returnTextFormatOfObject()
+            {
+                StringBuilder srBuilder = new StringBuilder();
+
+                srBuilder.AppendLine("srCarBrand" + srSplitValueSeperator + this.srCarBrand);
+                srBuilder.AppendLine("irCarPrice" + srSplitValueSeperator + irCarPrice);
+                srBuilder.AppendLine("srCarColor" + srSplitValueSeperator + srCarColor);
+                srBuilder.AppendLine("irCarWeight" + srSplitValueSeperator + irCarWeight);
+                srBuilder.AppendLine("srCarModel" + srSplitValueSeperator + srCarModel);
+                srBuilder.AppendLine("irProductionYear" + srSplitValueSeperator + irProductionYear);
+                srBuilder.AppendLine("lstRepairYears" + srSplitValueSeperator + string.Join(srSplitListSeperator, this.lstRepairYears));
+
+                return srBuilder.ToString();
+            }
+
+            public void loadFromFile(string srFileName, bool blNoFileName = false)
+            {
+                List<string> lstAllLines = new List<string>();
+
+                if(blNoFileName==false)
+                {
+                    lstAllLines = File.ReadAllLines(srFileName).ToList();
+                }
+                else
+                {
+                    lstAllLines = srFileName.Split("\r\n").ToList();
+                }
+
+                foreach (var vrLine in lstAllLines)
+                {
+                    var vrKey = vrLine.Split(srSplitValueSeperator);
+
+                    switch (vrKey[0])
+                    {
+                        case "srCarBrand":
+                            this.srCarBrand = vrKey[1];
+                            break;
+                        case "irCarPrice":
+                            this.irCarPrice = vrKey[1].toInt();
+                            break;
+                        case "srCarColor":
+                            this.srCarColor = vrKey[1];
+                            break;
+                        case "irCarWeight":
+                            this.irCarWeight = vrKey[1].toInt();
+                            break;
+                        case "srCarModel":
+                            this.srCarModel = vrKey[1];
+                            break;
+                        case "irProductionYear":
+                            this.irProductionYear = vrKey[1].toInt();
+                            break;
+                        case "lstRepairYears":
+                            this.lstRepairYears = vrKey[1].toIntList();
+                            break;
+                    }
+                }
+            }
+        }
+
+        public static void saveToFileCarsList(this List<cars> lstMyCars, string srFileName)
+        {
+            var vrFinalText = string.Join(srCarObjectSeperator, lstMyCars.Select(pr => pr.returnTextFormatOfObject()));
+
+            File.WriteAllText(srFileName, vrFinalText);
+        }
+
+        public static void loadCarsFromFile(this List<cars> lstMyCars, string srFileName)
+        {
+            var vrAllFile = File.ReadAllText(srFileName);
+
+            foreach (var vrPerCarObject in vrAllFile.Split(srCarObjectSeperator))
+            {
+                cars tempCar = new cars();
+                tempCar.loadFromFile(vrPerCarObject, true);
+                lstMyCars.Add(tempCar);
+            }
+        }
+    }
+}
